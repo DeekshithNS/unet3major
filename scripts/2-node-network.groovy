a3cd753a037ffffeeadf5ebed8393febb525ec47
DeekshithNS <deekshithns19@gmail.com>
2019-11-29 11:35:30 +0530

    aaa

--- scripts/2-node-network.groovy
+++ scripts/2-node-network.groovy
@@ -1,5 +1,5 @@
 import org.arl.fjage.*
-
+import org.arl.unet.sim.channels.*
 ///////////////////////////////////////////////////////////////////////////////
 // display documentation
 
@@ -15,14 +15,25 @@ Node C: tcp://localhost:1103, http://localhost:8083/
 ///////////////////////////////////////////////////////////////////////////////
 // simulator configuration
 
+channel.model = BasicAcousticChannel
+
+modem.dataRate = [2400, 2400].bps
+modem.frameLength = [2400/8, 2400/8].bytes
+modem.headerLength = 0
+modem.preambleDuration = 0
+modem.txDelay = 0
+
+
 platform = RealTimePlatform   // use real-time mode
 
 // run the simulation forever
 simulate {
-  node 'A', location: [ 0.km, 0.km, -15.m], web: 8081, api: 1101, stack: "$home/etc/setup"
-  node 'B', location: [ 0.5.km, 0.km, -15.m], web: 8082, api: 1102, stack: {
+  node 'A',address:1, location: [ 0.km, 0.km, -15.m], web: 8081, api: 1101, stack: "$home/etc/setup"
+  node 'B', address :2,location: [ 2.km, 0.km, -15.m], web: 8082, api: 1102, stack: {
   	container.add 'Floodmsg' , new Floodmsg()
   }
-   node 'C', location: [ 1.km, 0.km, -15.m], web: 8083, api: 1103, stack: "$home/etc/setup"
+   node 'C', address:3,location: [ 4.km, 0.km, -15.m], web: 8083, api: 1103, stack: {
+  	container.add 'Floodmsga' , new Floodmsga()
+  }
 
 }
