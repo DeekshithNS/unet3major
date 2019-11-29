d37303704dec1dc5061709f5fc717b5c918732df
DeekshithNS <deekshithns19@gmail.com>
2019-11-29 10:39:35 +0530

    AA

--- scripts/Floodmsg.groovy
+++ scripts/Floodmsg.groovy
@@ -4,37 +4,30 @@ import org.arl.unet.*
 class Floodmsg extends UnetAgent {
 
   @Override
-  void setup() {
-    // this method is called when the stack is initialized
-    // register services and capabilities that you provide here
- 
-    //subscribe uwlink
-    
-  }
-
-  @Override
   void startup() {
     // this method is called just after the stack is running
     // look up other agents and services here, as needed
     // subscribe to topics of interest to get notifications
 
-      subscribeForService(Services.PHYSICAL)
-      subscribeForService(Services.DATAGRAM)
-      //subscribe Topic('DATAGRAM')
+      
+      def phy = agentForService Services.PHYSICAL
+      subscribe topic(phy)
   }
 
-  @Override
-  Message processRequest(Message msg) {
-    // process requests supported by the agent, and return responses
-    // if request is not processed, return null
-
-    return null
-  }
-
-  @Override
   void processMessage(Message msg) {
     // process other messages, such as notifications here
     // if a message is not interesting, it can be safely just ignored
+
+ if (msg instanceof DatagramNtf ){
+
+      //int i = 10
+
+      while(1){
+
+      send new DatagramReq(recipient: msg.sender, to: Address.BROADCAST, protocol: Protocol.DATA)
+      //i = i-1
+}
   }
 
+}
 }
